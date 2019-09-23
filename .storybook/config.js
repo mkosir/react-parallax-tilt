d2fabfe81713f32ab36ba701f3047b395b636fde
mkosir <kosir.mar@gmail.com>
2019-09-23 08:57:42 +0200

    update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,7 @@
-import { addParameters, addDecorator, configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 
 addDecorator(withKnobs);
-
-addParameters({
-  options: {
-    showPanel: true,
-    panelPosition: 'right',
-  },
-});
 
 const req = require.context('../stories', true, /.story.(tsx?|ts?)$/);
 function loadStories() {
