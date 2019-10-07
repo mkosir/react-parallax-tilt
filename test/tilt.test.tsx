823e634910123f0969bf0a27d45ed51f67ef97f1
mkosir <kosir.mar@gmail.com>
2019-10-07 14:51:10 +0200

    add test

--- test/tilt.test.tsx
+++ test/tilt.test.tsx
@@ -111,7 +111,8 @@ describe("Tilt - (manual input) - Callback 'onMove' should return correct calcul
     wrapper.simulate('mousemove');
     wrapper.simulate('mouseleave');
     expect(wrapperSpyOnEnter.calledWith('mouseenter')).toEqual(true);
-    expect(wrapperSpyOnMove.calledWith(NaN, NaN, NaN, NaN, 0, NaN, 'mousemove')).toEqual(true);
+    console.log('Log: wrapperSpy.args[0]', wrapperSpyOnMove.args[0]);
+    expect(wrapperSpyOnMove.calledWith(0, -0, 0, -0, 0, 0, 'mousemove')).toEqual(true);
     expect(wrapperSpyOnLeave.calledWith('mouseleave')).toEqual(true);
   });
 
