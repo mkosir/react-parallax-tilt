f004934b82cc3c6788e7a5dc5a82079bcb14121b
mkosir <kosir.mar@gmail.com>
2019-12-09 23:21:58 +0100

    update storybook config

--- .storybook/config-theme.js
+++ .storybook/config-theme.js
@@ -1,10 +1,8 @@
 import { create } from '@storybook/theming/create';
-// import logo from './sb_logo.png';
 
 export default create({
   base: 'light',
 
   brandTitle: 'React Parallax Tilt 👀',
   brandUrl: 'https://github.com/mkosir/react-parallax-tilt',
-  // brandImage: logo,
 });
