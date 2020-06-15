f2290ca33506dc703bf3fe8714a12c948cb46558
mkosir <kosir.mar@gmail.com>
2020-06-15 21:01:35 +0200

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true, objectHashIgnoreUnknownHack: true }),
+    typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
@@ -46,7 +46,6 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
     terser({
-      sourcemap: true,
       output: { comments: false },
       compress: {
         pure_getters: true,
