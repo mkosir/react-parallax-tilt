d5dff8122c03580d6dca221d12d5efef13dc0470
mkosir <kosir.mar@gmail.com>
2022-04-11 21:43:16 +0200

    chore: upgrade Storybook to 6.x

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,27 @@
+import type { StorybookConfig } from '@storybook/core-common';
+
 // eslint-disable-next-line
 const path = require('path');
 
-module.exports = {
+const storybookConfig: StorybookConfig = {
+  // const storybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
   typescript: { reactDocgen: 'react-docgen' },
-
   core: {
     builder: 'webpack5',
   },
-
-  webpackFinal: async (config: any) => {
-    config.module.rules.push({
+  webpackFinal: async (config) => {
+    config.module!.rules!.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../'),
     });
 
     // Resolve absolute imports
-    config.resolve.modules.push(path.resolve(process.cwd(), 'src'));
+    config.resolve!.modules!.push(path.resolve(process.cwd(), 'src'));
 
     return config;
   },
 };
+
+module.exports = storybookConfig;
