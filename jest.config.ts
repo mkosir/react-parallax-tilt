ccc2dda789002057da171770d08db112d2f9eef3
mkosir <kosir.mar@gmail.com>
2022-04-11 10:58:06 +0200

    chore: update jest config

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+
+  transform: {
+    '\\.(ts|tsx)$': 'ts-jest',
+    '.+\\.(css|sass|scss)$': 'jest-transform-css',
+  },
+
+  testEnvironment: 'jsdom',
+
+  testRegex: '(/test/.*|\\.)\\.(test|spec)\\.(ts|tsx|js)$',
+
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+};
