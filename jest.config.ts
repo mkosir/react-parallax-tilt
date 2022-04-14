237b90eeceb0a4650fa9536470dc51805ea92595
mkosir <kosir.mar@gmail.com>
2022-04-14 09:05:57 +0200

    chore(jest): increase test coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,10 @@ const jestConfig: Config.InitialOptions = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
     },
   },
   preset: 'ts-jest',
