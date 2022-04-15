e2ccc8e7250c6429336a6afa3cd8798d488bba8d
mkosir <kosir.mar@gmail.com>
2022-04-15 21:26:55 +0200

    chore: refactor glare effect

---
+++ src/features/tilt/test/tiltOnTouchMove.test.tsx
@@ -0,0 +1,25 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import React from 'react';
+
+import { OnMoveParams } from '../../../react-parallax-tilt/types';
+import { TiltTest } from '../../../utils/TiltTest';
+
+describe('Tilt - onTouchMove', () => {
+  it('should call onMove prop when onTouchMove event is triggered', () => {
+    const onMove = jest.fn();
+
+    render(<TiltTest onMove={onMove} />);
+
+    fireEvent.touchMove(screen.getByText('test'), { touches: [{ pageX: 100, pageY: 50 }] });
+
+    expect(onMove).toHaveBeenLastCalledWith<[OnMoveParams]>({
+      tiltAngleX: 20,
+      tiltAngleY: -20,
+      tiltAngleXPercentage: 100,
+      tiltAngleYPercentage: -100,
+      glareAngle: 0,
+      glareOpacity: 0,
+      eventType: 'touchmove',
+    });
+  });
+});
