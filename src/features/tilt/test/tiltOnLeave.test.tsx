f511f9292f4b6c3def3df41e1c1103754761620a
mkosir <kosir.mar@gmail.com>
2022-04-15 13:10:56 +0200

    chore: refactor glare effect

---
+++ src/features/tilt/test/tiltOnLeave.test.tsx
@@ -0,0 +1,17 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { TiltTest } from '../../../utils/TiltTest';
+
+describe('Tilt - onLeave', () => {
+  it('should trigger onLeave event', () => {
+    const onLeave = jest.fn();
+
+    render(<TiltTest onLeave={onLeave} />);
+
+    userEvent.unhover(screen.getByText('test'));
+
+    expect(onLeave).toBeCalledWith('mouseleave');
+  });
+});
