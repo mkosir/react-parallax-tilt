bac76e87ad7305bbf9466753df0a3e7d36b760ee
mkosir <kosir.mar@gmail.com>
2022-04-15 21:31:58 +0200

    chore: refactor glare effect

--- src/features/tilt/test/tilt.test.tsx
+++ src/features/tilt/test/tilt.test.tsx
@@ -4,21 +4,10 @@ import React from 'react';
 
 import Tilt from '../../..';
 import { OnMoveParams } from '../../../react-parallax-tilt/types';
-import { ElementSizePosition } from '../../../utils/types';
 
 configure({ adapter: new Adapter() });
 
 describe("Tilt - (manual input) - Callback 'onMove' should return correct calculated values for defined manual input angles", () => {
-  let mockWrapperElSizePosition: ElementSizePosition;
-  beforeEach(() => {
-    mockWrapperElSizePosition = {
-      width: 100,
-      height: 100,
-      top: 0,
-      left: 0,
-    };
-  });
-
   it('Tilt events', () => {
     jest.useFakeTimers();
 
@@ -43,24 +32,5 @@ describe("Tilt - (manual input) - Callback 'onMove' should return correct calcul
       eventType: 'mousemove',
     });
     expect(onLeave).toBeCalledWith('mouseleave');
-  });
-
-  it('Tilt disabled', () => {
-    const onMove = jest.fn();
-
-    const wrapper = mount<Tilt>(<Tilt tiltEnable={false} tiltMaxAngleX={60} tiltMaxAngleY={60} onMove={onMove} />);
-
-    wrapper.instance()['wrapperEl'].size = mockWrapperElSizePosition;
-    wrapper.simulate('touchmove', { touches: [{ pageX: 100, pageY: 50 }] });
-
-    expect(onMove).toBeCalledWith<[OnMoveParams]>({
-      tiltAngleX: 0,
-      tiltAngleY: 0,
-      tiltAngleXPercentage: 0,
-      tiltAngleYPercentage: 0,
-      glareAngle: 0,
-      glareOpacity: 0,
-      eventType: 'touchmove',
-    });
   });
 });
---
+++ src/features/tilt/test/tiltDisable.test.tsx
@@ -0,0 +1,25 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import React from 'react';
+
+import { OnMoveParams } from '../../../react-parallax-tilt/types';
+import { TiltTest } from '../../../utils/TiltTest';
+
+describe('Tilt - Disable', () => {
+  it('should not calculate tilt when disabled', () => {
+    const onMove = jest.fn();
+
+    render(<TiltTest tiltEnable={false} onMove={onMove} />);
+
+    fireEvent.touchMove(screen.getByText('test'), { touches: [{ pageX: 100, pageY: 50 }] });
+
+    expect(onMove).toHaveBeenLastCalledWith<[OnMoveParams]>({
+      tiltAngleX: 0,
+      tiltAngleY: 0,
+      tiltAngleXPercentage: 0,
+      tiltAngleYPercentage: 0,
+      glareAngle: 0,
+      glareOpacity: 0,
+      eventType: 'touchmove',
+    });
+  });
+});
