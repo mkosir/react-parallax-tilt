c7b9d4dc31cb57ffbb8120b0c33affa9ab94eb57
mkosir <kosir.mar@gmail.com>
2022-04-15 11:08:00 +0200

    chore: refactor glare effect

--- src/features/tilt/test/tilt.test.tsx
+++ src/features/tilt/test/tilt.test.tsx
@@ -19,31 +19,6 @@ describe("Tilt - (manual input) - Callback 'onMove' should return correct calcul
     };
   });
 
-  it('Tilt - reverse', () => {
-    const onMove = jest.fn();
-
-    const wrapper = mount<Tilt>(
-      <Tilt
-        tiltReverse={true}
-        tiltMaxAngleX={60}
-        tiltMaxAngleY={60}
-        tiltAngleXManual={60}
-        tiltAngleYManual={45}
-        onMove={onMove}
-      />,
-    );
-    wrapper.simulate('mousemove');
-    expect(onMove).toBeCalledWith<[OnMoveParams]>({
-      tiltAngleX: -60,
-      tiltAngleY: -45,
-      tiltAngleXPercentage: -100,
-      tiltAngleYPercentage: -75,
-      glareAngle: 0,
-      glareOpacity: 0,
-      eventType: 'mousemove',
-    });
-  });
-
   it('Tilt - track on window', () => {
     const onMove = jest.fn();
 
---
+++ src/features/tilt/test/tiltReverse.test.tsx
@@ -0,0 +1,35 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { OnMoveParams } from '../../../react-parallax-tilt/types';
+import { TiltTest } from '../../../utils/TiltTest';
+
+describe('Tilt - Reverse', () => {
+  it('should calculate reverse tilt', () => {
+    const onMove = jest.fn();
+
+    render(
+      <TiltTest
+        tiltReverse={true}
+        tiltMaxAngleX={60}
+        tiltMaxAngleY={60}
+        tiltAngleXManual={60}
+        tiltAngleYManual={45}
+        onMove={onMove}
+      />,
+    );
+
+    userEvent.hover(screen.getByText('test'));
+
+    expect(onMove).toBeCalledWith<[OnMoveParams]>({
+      tiltAngleX: -60,
+      tiltAngleY: -45,
+      tiltAngleXPercentage: -100,
+      tiltAngleYPercentage: -75,
+      glareAngle: 0,
+      glareOpacity: 0,
+      eventType: 'mousemove',
+    });
+  });
+});
