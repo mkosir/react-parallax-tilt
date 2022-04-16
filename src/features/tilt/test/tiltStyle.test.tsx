b7a9f876e91c30d4c0b8fad92fd31f7613795afe
mkosir <kosir.mar@gmail.com>
2022-04-16 21:23:06 +0200

    chore: refactor glare effect

--- src/features/tilt/test/tiltStyle.test.tsx
+++ src/features/tilt/test/tiltStyle.test.tsx
@@ -1,27 +1,31 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 
 import { TiltTest } from '../../../utils/TiltTest';
 
 describe('Tilt - Style', () => {
-  it('should update tilt style', () => {
+  it('should update tilt style on hovering/touch tilt element', () => {
     jest.useFakeTimers();
 
     const onMove = jest.fn();
 
     const { container } = render(<TiltTest onMove={onMove} glareEnable={true} />);
 
-    const tiltElement = screen.getByText('test');
+    // eslint-disable-next-line
+    const tiltElement = container.firstElementChild as Element;
 
-    userEvent.hover(tiltElement);
+    const positionStart = [{ pageX: 50, pageY: 50 }];
+    const positionEnd = [{ pageX: 100, pageY: 100 }];
+
+    fireEvent.touchStart(tiltElement, { touches: positionStart });
+    fireEvent.touchMove(tiltElement, { touches: positionEnd });
 
     jest.runAllTimers();
 
-    expect(container.firstChild).toHaveStyle({
+    expect(tiltElement).toHaveStyle({
       'will-change': 'transform',
       transition: 'all 400ms cubic-bezier(.03,.98,.52,.99)',
-      transform: 'perspective(1000px) rotateX(NaNdeg) rotateY(NaNdeg) scale3d(1,1,1)',
+      transform: 'perspective(1000px) rotateX(20deg) rotateY(-20deg) scale3d(1,1,1)',
     });
   });
 });
