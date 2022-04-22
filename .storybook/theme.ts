bfdb55b250c34e2bfc86322290bca0b68884eb6a
mkosir <kosir.mar@gmail.com>
2022-04-22 23:31:17 +0200

    chore: upgrade Storybook to 6.x

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,5 +4,4 @@ export const theme = create({
   base: 'light',
   brandTitle: 'React Parallax Tilt 👀',
   brandUrl: 'https://github.com/mkosir/react-parallax-tilt',
-  // brandImage: 'https://raw.githubusercontent.com/mkosir/react-parallax-tilt/master/logo.png',
 });
