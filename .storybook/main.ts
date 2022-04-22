4dff428f267d50385b083e0a53b0495183f3d74a
mkosir <kosir.mar@gmail.com>
2022-04-22 13:43:45 +0200

    chore: upgrade Storybook to 6.x

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 
 import { StorybookConfig } from '@storybook/core-common';
+// import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 
 const storybookConfig: StorybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
@@ -17,6 +18,13 @@ const storybookConfig: StorybookConfig = {
 
     // Resolve absolute imports
     config.resolve!.modules!.push(path.resolve(process.cwd(), 'src'));
+
+    // Plugin 'TsconfigPathsPlugin' issue with Webpack v5 - https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/60
+    // config.resolve!.plugins! = [
+    //   new TsconfigPathsPlugin({
+    //     configFile: path.resolve(__dirname, '../tsconfig.dev.json'),
+    //   }),
+    // ];
 
     return config;
   },
