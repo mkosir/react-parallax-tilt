11713b04725a295821129947c82cf8cecec0ca20
mkosir <kosir.mar@gmail.com>
2022-04-23 08:07:12 +0200

    chore(Glare): update story

--- stories/TiltManualInput/TiltManualInput.demotab.tsx
+++ stories/TiltManualInput/TiltManualInput.demotab.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { Joystick } from 'react-joystick-component';
-import { IJoystickUpdateEvent } from 'react-joystick-component/build/lib/Joystick';
+import React from 'react';
+// import { Joystick } from 'react-joystick-component';
+// import { IJoystickUpdateEvent } from 'react-joystick-component/build/lib/Joystick';
 
-import Tilt from '../../src';
+// import Tilt from '../../src';
 import './TiltManualInput.demotab.scss';
 
 const TiltManualInput = () => {
-  const [[manualTiltAngleX, manualTiltAngleY], setManualTiltAngle] = useState([0, 0]);
+  // const [[manualTiltAngleX, manualTiltAngleY], setManualTiltAngle] = useState([0, 0]);
 
-  const onMove = (stick: IJoystickUpdateEvent) => {
-    setManualTiltAngle([stick.y ?? 0, stick.x ?? 0]);
-  };
+  // const onMove = (stick: IJoystickUpdateEvent) => {
+  //   setManualTiltAngle([stick.y ?? 0, stick.x ?? 0]);
+  // };
 
-  const onStop = () => {
-    setManualTiltAngle([0, 0]);
-  };
+  // const onStop = () => {
+  //   setManualTiltAngle([0, 0]);
+  // };
 
   return (
     <div className="tilt-manual-input">
-      <Tilt tiltAngleXManual={manualTiltAngleX} tiltAngleYManual={manualTiltAngleY} glareEnable={true}>
+      {/* <Tilt tiltAngleXManual={manualTiltAngleX} tiltAngleYManual={manualTiltAngleY} glareEnable={true}>
         <div className="react-parallax-tilt">
           <div>Axis x: {manualTiltAngleX.toFixed(0)}°</div>
           <div>Axis y: {manualTiltAngleY.toFixed(0)}°</div>
         </div>
-      </Tilt>
+      </Tilt> */}
       <div className="manual-input">
-        <Joystick baseColor="darkgreen" stickColor="black" move={onMove} stop={onStop} />
+        {/* <Joystick baseColor="darkgreen" stickColor="black" move={onMove} stop={onStop} /> */}
       </div>
     </div>
   );
