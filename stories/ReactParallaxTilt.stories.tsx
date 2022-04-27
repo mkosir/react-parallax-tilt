65c2a28e48ca4d78ff9608baf4165c884ee44d76
mkosir <kosir.mar@gmail.com>
2022-04-27 22:54:32 +0200

    chore(Glare): update story

--- stories/ReactParallaxTilt.stories.tsx
+++ stories/ReactParallaxTilt.stories.tsx
@@ -1,29 +1,29 @@
 import type { Meta } from '@storybook/react';
 import React from 'react';
 
-import DefaultC from './Default/_Default';
-import EventsAllC from './EventsAll/_EventsAll';
-import EventTiltAngleC from './EventTiltAngle/_EventTiltAngle';
-import FlipPageC from './FlipPage/_FlipPage';
-import FlipVHC from './FlipVH/_FlipVH';
-import GlareEffectC from './GlareEffect/_GlareEffect';
-import GlareEffect360C from './GlareEffect360/_GlareEffect360';
-import GlareEffectNoTiltC from './GlareEffectNoTilt/_GlareEffectNoTilt';
-import GyroscopeC from './Gyroscope/_Gyroscope';
-import InitialTiltC from './InitialTilt/_InitialTilt';
-import KeepFloatingC from './KeepFloating/_KeepFloating';
-import MultipleTiltC from './MultipleTilt/_MultipleTilt';
-import MultipleTiltScrollC from './MultipleTiltScroll/_MultipleTiltScroll';
-import ParallaxEffectC from './ParallaxEffect/_ParallaxEffect';
-import ParallaxEffectGlareScaleC from './ParallaxEffectGlareScale/_ParallaxEffectGlareScale';
-import ParallaxEffectImgC from './ParallaxEffectImg/_ParallaxEffectImg';
-import ReverseTiltC from './ReverseTilt/_ReverseTilt';
-import ScaleNoTiltC from './ScaleNoTilt/_ScaleNoTilt';
-import TiltDisableAxisC from './TiltDisableAxis/_TiltDisableAxis';
-import TiltImgC from './TiltImg/_TiltImg';
-import TiltManualInputC from './TiltManualInput/_TiltManualInput';
-import TiltScaleC from './TiltScale/_TiltScale';
-import TrackOnWindowC from './TrackOnWindow/_TrackOnWindow';
+import { _Default } from './Default/_Default';
+import { _EventsAll } from './EventsAll/_EventsAll';
+import { _EventTiltAngle } from './EventTiltAngle/_EventTiltAngle';
+import { _FlipPage } from './FlipPage/_FlipPage';
+import { _FlipVH } from './FlipVH/_FlipVH';
+import { _GlareEffect } from './GlareEffect/_GlareEffect';
+import { _GlareEffect360 } from './GlareEffect360/_GlareEffect360';
+import { _GlareEffectNoTilt } from './GlareEffectNoTilt/_GlareEffectNoTilt';
+import { _Gyroscope } from './Gyroscope/_Gyroscope';
+import { _InitialTilt } from './InitialTilt/_InitialTilt';
+import { _KeepFloating } from './KeepFloating/_KeepFloating';
+import { _MultipleTilt } from './MultipleTilt/_MultipleTilt';
+import { _MultipleTiltScroll } from './MultipleTiltScroll/_MultipleTiltScroll';
+import { _ParallaxEffect } from './ParallaxEffect/_ParallaxEffect';
+import { _ParallaxEffectGlareScale } from './ParallaxEffectGlareScale/_ParallaxEffectGlareScale';
+import { _ParallaxEffectImg } from './ParallaxEffectImg/_ParallaxEffectImg';
+import { _ReverseTilt } from './ReverseTilt/_ReverseTilt';
+import { _ScaleNoTilt } from './ScaleNoTilt/_ScaleNoTilt';
+import { _TiltDisableAxis } from './TiltDisableAxis/_TiltDisableAxis';
+import { _TiltImg } from './TiltImg/_TiltImg';
+import { _TiltManualInput } from './TiltManualInput/_TiltManualInput';
+import { _TiltScale } from './TiltScale/_TiltScale';
+import { _TrackOnWindow } from './TrackOnWindow/_TrackOnWindow';
 
 import './ReactParallaxTilt.scss';
 
@@ -33,69 +33,69 @@ const meta: Meta = {
 
 export default meta;
 
-export const Default = () => <DefaultC />;
+export const Default = () => <_Default />;
 
-export const KeepFloating = () => <KeepFloatingC />;
+export const KeepFloating = () => <_KeepFloating />;
 KeepFloating.storyName = 'Keep floating';
 
-export const ReverseTilt = () => <ReverseTiltC />;
+export const ReverseTilt = () => <_ReverseTilt />;
 ReverseTilt.storyName = 'Reverse tilt';
 
-export const Scale = () => <TiltScaleC />;
+export const Scale = () => <_TiltScale />;
 
-export const ScaleNoTilt = () => <ScaleNoTiltC />;
+export const ScaleNoTilt = () => <_ScaleNoTilt />;
 ScaleNoTilt.storyName = 'Scale - No tilt';
 
-export const TiltDisableAxis = () => <TiltDisableAxisC />;
+export const TiltDisableAxis = () => <_TiltDisableAxis />;
 TiltDisableAxis.storyName = 'Disable x/y axis';
 
-export const InitialTilt = () => <InitialTiltC />;
+export const InitialTilt = () => <_InitialTilt />;
 InitialTilt.storyName = 'Initial tilt';
 
-export const FlipVH = () => <FlipVHC />;
+export const FlipVH = () => <_FlipVH />;
 FlipVH.storyName = 'Flip vertically/horizontally';
 
-export const FlipPage = () => <FlipPageC />;
+export const FlipPage = () => <_FlipPage />;
 FlipPage.storyName = 'Flip page';
 
-export const ParallaxEffect = () => <ParallaxEffectC />;
+export const ParallaxEffect = () => <_ParallaxEffect />;
 ParallaxEffect.storyName = 'Parallax effect';
 
-export const ParallaxEffectGlareScale = () => <ParallaxEffectGlareScaleC />;
+export const ParallaxEffectGlareScale = () => <_ParallaxEffectGlareScale />;
 ParallaxEffectGlareScale.storyName = 'Parallax effect - Glare & Scale';
 
-export const ParallaxEffectImg = () => <ParallaxEffectImgC />;
+export const ParallaxEffectImg = () => <_ParallaxEffectImg />;
 ParallaxEffectImg.storyName = 'Parallax effect image';
 
-export const TiltImg = () => <TiltImgC />;
+export const TiltImg = () => <_TiltImg />;
 TiltImg.storyName = 'Tilt image';
 
-export const GlareEffect = () => <GlareEffectC />;
+export const GlareEffect = () => <_GlareEffect />;
 GlareEffect.storyName = 'Glare effect';
 
-export const GlareEffectNoTilt = () => <GlareEffectNoTiltC />;
+export const GlareEffectNoTilt = () => <_GlareEffectNoTilt />;
 GlareEffectNoTilt.storyName = 'Glare effect - No tilt';
 
-export const GlareEffect360 = () => <GlareEffect360C />;
+export const GlareEffect360 = () => <_GlareEffect360 />;
 GlareEffect360.storyName = 'Glare effect - 360';
 
-export const TrackOnWindow = () => <TrackOnWindowC />;
+export const TrackOnWindow = () => <_TrackOnWindow />;
 TrackOnWindow.storyName = 'Track on window';
 
-export const TiltManualInput = () => <TiltManualInputC />;
+export const TiltManualInput = () => <_TiltManualInput />;
 TiltManualInput.storyName = 'Tilt manual input';
 
-export const Gyroscope = () => <GyroscopeC />;
+export const Gyroscope = () => <_Gyroscope />;
 Gyroscope.storyName = 'Gyroscope tilt';
 
-export const MultipleTilt = () => <MultipleTiltC />;
+export const MultipleTilt = () => <_MultipleTilt />;
 MultipleTilt.storyName = 'Multiple tilt';
 
-export const MultipleTiltScroll = () => <MultipleTiltScrollC />;
+export const MultipleTiltScroll = () => <_MultipleTiltScroll />;
 MultipleTiltScroll.storyName = 'Multiple tilt - Scroll';
 
-export const EventTiltAngle = () => <EventTiltAngleC />;
+export const EventTiltAngle = () => <_EventTiltAngle />;
 EventTiltAngle.storyName = 'Event - Tilt angle';
 
-export const EventsAll = () => <EventsAllC />;
+export const EventsAll = () => <_EventsAll />;
 EventsAll.storyName = 'Event - All';
