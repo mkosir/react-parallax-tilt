b8e9b4998183ecdde697469ad37b23dab2aa6c06
mkosir <kosir.mar@gmail.com>
2022-05-02 15:26:09 +0200

    chore(eslint): update import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ const eslintConfig = {
 
   overrides: [
     {
-      files: ['stories/**/*.demozap.tsx'],
+      files: ['stories/**/*.demozap.*'],
       rules: {
         'import/no-default-export': 'off',
       },
