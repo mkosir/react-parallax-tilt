8c323836a52bf6d056ae2391f5f4dee05a1eace4
mkosir <kosir.mar@gmail.com>
2022-06-13 15:26:45 +0200

    chore(jest): increase test coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import { Config } from '@jest/types';
+import { Config } from 'jest';
 
-const jestConfig: Config.InitialOptions = {
+const jestConfig: Config = {
   testEnvironment: 'jsdom',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
