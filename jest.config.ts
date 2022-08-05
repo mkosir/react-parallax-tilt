6008d716c093ef005440e54e8f6d3ca88ca0ea2b
mkosir <kosir.mar@gmail.com>
2022-08-05 10:38:58 +0200

    chore(jest): extend config

--- jest.config.ts
+++ jest.config.ts
@@ -9,10 +9,10 @@ const jestConfig: Config = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   coverageThreshold: {
     global: {
-      branches: 95,
-      functions: 95,
-      lines: 95,
-      statements: 95,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
