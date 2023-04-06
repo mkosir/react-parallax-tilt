fd43f4830148bdea543d031bb34d4253ccaefe6f
mkosir <kosir.mar@gmail.com>
2023-04-06 17:20:16 +0200

    chore: upgrade Storybook to 7.x

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,18 @@
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    // Description toggle
-    // expanded: true,
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+import { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      // Description toggle
+      // expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
 };
+
+// eslint-disable-next-line import/no-default-export
+export default preview;
