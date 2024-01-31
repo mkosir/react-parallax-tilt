42a173b6b87444b75c18921843d647fce9b7009d
mkosir <kosir.mar@gmail.com>
2024-01-31 15:48:16 +0100

    chore(deps): update dependencies

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 // import { OnMoveParams } from 'index';
 
-test('should get max values of move params when mouse is positioned at border/corner of tilt element', async ({
+test('should get max values of move params rendered when mouse is positioned at top left corner of tilt element', async ({
   page,
 }) => {
   await page.goto('/');
@@ -15,24 +15,39 @@ test('should get max values of move params when mouse is positioned at border/co
   await expect(content.getByTestId('tiltAngleY')).toHaveText('20.00° / 100.00%');
   await expect(content.getByTestId('glareAngle')).toHaveText('-45.00°');
   await expect(content.getByTestId('glareOpacity')).toHaveText('0.00');
-
-  // const paramsString = await content.getByTestId('params').innerText();
-  // const params = JSON.parse(paramsString) as OnMoveParams;
-
-  // await content.getByTestId('topRight').hover({ position: { x: 10, y: 1 } });
-  // await expect(content.getByTestId('tiltAngleX')).toHaveText('-20.00° / -100.00%');
-  // await expect(content.getByTestId('tiltAngleY')).toHaveText('-20.00° / -100.00%');
-
-  // await content.getByTestId('bottomRight').hover({ position: { x: 19, y: 19 } });
-  // await expect(content.getByTestId('tiltAngleX')).toHaveText('20.00° / 100.00%');
-  // await expect(content.getByTestId('tiltAngleY')).toHaveText('-20.00° / -100.00%');
-
-  // await content.getByTestId('bottomLeft').hover({ position: { x: 1, y: 19 } });
-  // await expect(content.getByTestId('tiltAngleX')).toHaveText('20.00° / 100.00%');
-  // await expect(content.getByTestId('tiltAngleY')).toHaveText('20.00° / 100.00%');
 });
 
-// test('should x when y', async ({ page }) => {
+// test('222 should get max values of move params when mouse is positioned at border/corner of tilt element', async ({
+//   page,
+// }) => {
+//   await page.goto('/');
+//   await page.getByRole('link', { name: 'Event - Params' }).click();
+
+//   const content = page.frameLocator('iframe[title="storybook-preview-iframe"]');
+
+//   await content.getByTestId('topLeft').hover({ position: { x: 10, y: 10 } });
+//   await expect(content.getByTestId('tiltAngleX')).toHaveText('-20.00° / -100.00%');
+//   await expect(content.getByTestId('tiltAngleY')).toHaveText('20.00° / 100.00%');
+//   await expect(content.getByTestId('glareAngle')).toHaveText('-45.00°');
+//   await expect(content.getByTestId('glareOpacity')).toHaveText('0.00');
+
+// const paramsString = await content.getByTestId('params').innerText();
+// const params = JSON.parse(paramsString) as OnMoveParams;
+
+// await content.getByTestId('topRight').hover({ position: { x: 10, y: 1 } });
+// await expect(content.getByTestId('tiltAngleX')).toHaveText('-20.00° / -100.00%');
+// await expect(content.getByTestId('tiltAngleY')).toHaveText('-20.00° / -100.00%');
+
+// await content.getByTestId('bottomRight').hover({ position: { x: 19, y: 19 } });
+// await expect(content.getByTestId('tiltAngleX')).toHaveText('20.00° / 100.00%');
+// await expect(content.getByTestId('tiltAngleY')).toHaveText('-20.00° / -100.00%');
+
+// await content.getByTestId('bottomLeft').hover({ position: { x: 1, y: 19 } });
+// await expect(content.getByTestId('tiltAngleX')).toHaveText('20.00° / 100.00%');
+// await expect(content.getByTestId('tiltAngleY')).toHaveText('20.00° / 100.00%');
+// });
+
+// test('should get half of max value when mouse is positioned in the middle of tilt element', async ({ page }) => {
 //   await page.goto('http://localhost:9009');
 //   await page.getByRole('link', { name: 'Event - Params' }).click();
 
