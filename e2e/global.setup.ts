6f7c45f91c2106da845f83f37bd0d3a41a260e0e
mkosir <kosir.mar@gmail.com>
2024-02-02 11:37:45 +0100

    chore(deps): update dependencies

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -1,17 +1,5 @@
 import { test as setup } from '@playwright/test';
 
-// setup('do login', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByLabel('User Name').fill('user');
-//   await page.getByLabel('Password').fill('password');
-//   await page.getByText('Sign in').click();
-
-//   // Wait until the page actually signs in.
-//   await expect(page.getByText('Hello, user!')).toBeVisible();
-
-//   await page.context().storageState({ path: STORAGE_STATE });
-// });
-
 setup("remove Storybook 'what's new' popup", async ({ page }) => {
   await page.goto('/');
   await page.getByRole('button', { name: 'Dismiss notification' }).click();
