591c470b11826db7393c864ac21bfb44452de844
mkosir <kosir.mar@gmail.com>
2024-02-02 11:46:48 +0100

    chore(deps): update dependencies

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -2,5 +2,8 @@ import { test as setup } from '@playwright/test';
 
 setup("remove Storybook 'what's new' popup", async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('button', { name: 'Dismiss notification' }).click();
+
+  if (await page.isVisible("button:has-text('Dismiss notification')")) {
+    await page.getByRole('button', { name: 'Dismiss notification' }).click();
+  }
 });
