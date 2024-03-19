d445d8696ebd48099379126f266223701bfa38da
mkosir <kosir.mar@gmail.com>
2024-03-19 18:30:23 +0100

    fix(deps): update dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,17 +4,19 @@ import { StorybookConfig } from '@storybook/react-webpack5';
 
 const storybookConfig: StorybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
-  typescript: { reactDocgen: 'react-docgen-typescript' },
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
+
   webpackFinal: (config) => {
     // Resolve absolute imports
     config.resolve?.modules?.push(path.resolve(process.cwd(), 'src'));
 
     return config;
   },
+
+  addons: ['@storybook/addon-webpack5-compiler-swc'],
 };
 
 // eslint-disable-next-line import/no-default-export
--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,7 +1,16 @@
-import { addons } from '@storybook/addons';
+import { addons } from '@storybook/manager-api';
 
 import { theme } from './theme';
 
 addons.setConfig({
   theme,
+  toolbar: {
+    title: { hidden: false },
+    zoom: { hidden: false },
+    eject: { hidden: false },
+    copy: { hidden: false },
+    fullscreen: { hidden: false },
+  },
+  bottomPanelHeight: 50,
+  panelPosition: false,
 });
