8a59b3c3881c9747eca4dc24c9c09b7febbff43b
mkosir <mkosir@users.noreply.github.com>
2024-06-13 06:28:41 +0000

    Deploying to gh-pages from @ mkosir/react-parallax-tilt@ebbb65c230d0087267bc01a79db9c6a1ab76d0eb 🚀

--- sb-manager/index.js
+++ sb-manager/index.js
@@ -1,1 +1,1 @@
-import{Provider,Root,renderStorybookUI}from"./chunk-6GQRNYH2.js";import"./chunk-4FMBEAHW.js";import"./chunk-GUVK2GTO.js";import"./chunk-BLWCBWKL.js";import"./chunk-B3YDJJJH.js";import"./chunk-ZR5JZWHI.js";export{Provider,Root,renderStorybookUI};
+import{Provider,Root,renderStorybookUI}from"./chunk-IZFICJR3.js";import"./chunk-J2NPMGLZ.js";import"./chunk-GUVK2GTO.js";import"./chunk-BLWCBWKL.js";import"./chunk-B3YDJJJH.js";import"./chunk-ZR5JZWHI.js";export{Provider,Root,renderStorybookUI};
