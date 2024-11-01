ab29ee486cfcf74a5fda5596d383d7dd04af2113
mkosir <kosir.mar@gmail.com>
2024-11-01 20:11:35 +0100

    chore(deps): update dependencies

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -5,6 +5,11 @@ setup("remove Storybook 'what's new' popup", async ({ page }) => {
 
   await page.getByRole('link', { name: 'Keep floating' }).isVisible();
 
+  console.log(
+    '🔎 Log ~ setup ~ await page.isVisible("text=Learn whats new in Storybook"):',
+    await page.isVisible("text='Learn what's new in Storybook'"),
+  );
+
   if (await page.isVisible("text='Learn what's new in Storybook'")) {
     await page.getByRole('button', { name: 'Dismiss notification' }).click();
   }
