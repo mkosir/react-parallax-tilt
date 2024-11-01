3acdf32553d3e1abade74e8a1bf7426090569c7e
mkosir <kosir.mar@gmail.com>
2024-11-01 20:51:32 +0100

    chore(deps): update dependencies

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -3,31 +3,11 @@ import { test as setup } from '@playwright/test';
 setup("remove Storybook 'what's new' popup", async ({ page }) => {
   await page.goto('/');
 
-  await page.getByRole('link', { name: 'Keep floating' }).isVisible();
-
-  console.log(
-    '🔎 Log ~ setup ~ page.getByRole(link, { name: Keep floating }).isVisible():',
-    await page.getByRole('link', { name: 'Keep floating' }).isVisible(),
-  );
-
+  // Wait for sidebar to load.
   await page.getByRole('link', { name: 'Keep floating' }).waitFor({
     state: 'visible',
     timeout: 5000,
   });
-
-  console.log(
-    '🔎 Log ~ setup ~ page.getByRole(link, { name: Keep floating }).isVisible():',
-    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
-    await page.getByRole('link', { name: 'Keep floating' }).waitFor({
-      state: 'visible',
-      timeout: 5000,
-    }),
-  );
-
-  console.log(
-    '🔎 Log ~ setup ~ await page.isVisible("text=Learn whats new in Storybook"):',
-    await page.isVisible("text='Learn what's new in Storybook'"),
-  );
 
   if (await page.isVisible("text='Learn what's new in Storybook'")) {
     await page.getByRole('button', { name: 'Dismiss notification' }).click();
