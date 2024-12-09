d493f9b4f233f532b932fe300f5f8283ca32d180
mkosir <kosir.mar@gmail.com>
2024-12-09 10:52:03 +0100

    chore(deps): update dependencies

---
+++ knip.ts
@@ -0,0 +1,8 @@
+import type { KnipConfig } from 'knip';
+
+const knipConfig: KnipConfig = {
+  entry: ['src/index.ts'],
+};
+
+// eslint-disable-next-line import/no-default-export
+export default knipConfig;
