44645573c8555c6d6a8890020758bf641d21e350
mkosir <kosir.mar@gmail.com>
2024-12-10 13:26:25 +0100

    chore: update knip paths

--- knip.ts
+++ knip.ts
@@ -2,6 +2,14 @@ import type { KnipConfig } from 'knip';
 
 const knipConfig: KnipConfig = {
   entry: ['src/index.ts'],
+  ignore: [
+    'dist/**',
+    'compiled/**',
+    'build-storybook-static/**',
+    'test-unit-report/**',
+    'test-e2e-results/**',
+    'test-e2e-report/**',
+  ],
 };
 
 // eslint-disable-next-line import/no-default-export
