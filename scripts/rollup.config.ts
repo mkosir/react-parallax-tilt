0f3078a9576945c55d67ce59a8633e82069c2145
mkosir <kosir.mar@gmail.com>
2025-05-12 08:45:43 +0200

    chore(deps): update dependencies

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -4,5 +4,5 @@ import { LEGACY_CONFIG, MODERN_CONFIG } from './rollupConfigs';
 
 const rollupConfig = defineConfig([...LEGACY_CONFIG, ...MODERN_CONFIG]);
 
-// eslint-disable-next-line
+// eslint-disable-next-line import-x/no-default-export
 export default rollupConfig;
