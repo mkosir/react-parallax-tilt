7a7f8a68d479dc8cbedacc3bafb37dc3c742a32b
mkosir <kosir.mar@gmail.com>
2025-06-19 11:21:20 +0200

    chore(deps): storybook use vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,33 +1,18 @@
-import path from 'path';
-
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const storybookConfig: StorybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
     options: {},
   },
-
-  // JSX transform no longer requires importing React explicitly in every file.
-  swc: () => ({
-    jsc: {
-      transform: {
-        react: {
-          runtime: 'automatic',
-        },
-      },
-    },
-  }),
-
-  webpackFinal: (config) => {
-    // Resolve absolute imports
-    config.resolve?.modules?.push(path.resolve(process.cwd(), 'src'));
-
-    return config;
+  viteFinal: (storybookConfig) => {
+    return {
+      ...storybookConfig,
+      plugins: [...(storybookConfig.plugins ?? []), tsconfigPaths()],
+    };
   },
-
-  addons: ['@storybook/addon-webpack5-compiler-swc'],
 };
 
 // eslint-disable-next-line import-x/no-default-export
